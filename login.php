418d8dd9938f01a10e76750cb4605aa68f642eb3
GiorgiaParma <124531718+GiorgiaParma@users.noreply.github.com>
2023-05-30 17:26:30 +0200

    commit 3 martedi

--- login.php
+++ login.php
@@ -2,11 +2,12 @@
 	 $db_servername = "localhost";
 	 $db_username = "root";
 	 $db_password = "";
-	require("./data/dati_connessione_db.php");
+	require("./dati/dati_connessione_db.php");
 	if (isset($_POST["username"])) {$username = $_POST["username"];} else {$username = "";}
 	if (isset($_POST["password"])) {$password = $_POST["password"];} else {$password = "";}
 	if (isset($_POST["tipologia"])) {$tipologia = $_POST["tipologia"];} else {$tipologia = "";}
 ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -17,7 +18,6 @@
 	<link rel="stylesheet" href="login.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<title>GiCa & CO - Login</title>
 	<link rel="stylesheet" type="text/css" href="login.css">
-	<link rel="" type="" href="index.php">
 </head>
 
 <body>
@@ -41,6 +41,31 @@
 		</form>
 	</div>
 
+	<?php
+			if (isset($_POST["username"]) and isset($_POST["password"]) and isset($_POST["tipologia"])) {
+				$conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
+				if($conn->connect_error){
+					die("<p>Connessione al server non riuscita: ".$conn->connect_error."</p>");
+				}
+
+				$myquery = "SELECT username, password 
+							FROM $tipologia 
+							WHERE username='$username'
+								AND password='$password'";
+
+				$ris = $conn->query($myquery) or die("<p>Query fallita! ".$conn->error."</p>");
+
+				if($ris->num_rows == 0){
+					echo "<p>Utente non trovato o password errata</p>";
+					$conn->close();
+				} 
+				else {
+					echo "<p>Utente trovato</p>";
+				}
+			}
+
+		?>	
+		
 	<?php 
 		include('pagine/footer.php')
 	?>
