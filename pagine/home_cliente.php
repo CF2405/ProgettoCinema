5425086d4627dc8218f71f2053eb75a55bb7b76d
GiorgiaParma <124531718+GiorgiaParma@users.noreply.github.com>
2023-06-02 17:09:34 +0200

    commi2 venerdi

--- pagine/home_cliente.php
+++ pagine/home_cliente.php
@@ -22,18 +22,42 @@
                     <h1 class="titolo">GICAandCO</h1>
                     <img src="../immagini/logo.jpg" alt="" class="logo"  width="60" height="60">
         </div> 
-            
-          
+       
+        <div class="topnav">
+            <a href="pagine\logout.php">Log Out</a>
+            <a href="index.php">Home Page</a>
+        </div>           
 </div>
 
-<div class="main">
+
+    <div class="row">
+    <div class="column">
+        <h2>Inizia a guardare: 'Oppenheimer'</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/it/thumb/4/4a/Oppenheimer_%28film%29.jpg/1200px-Oppenheimer_%28film%29.jpg" alt="">
+        <p>Il fisico J Robert Oppenheimer lavora con una squadra di scienziati durante il Progetto Manhattan, che porta allo sviluppo della bomba atomica.</p>
+    </div>
+    
+    <div class="column">
+    <h2>Inizia a guardare: 'Dunkirk'</h2>
+        <img src="https://images.squarespace-cdn.com/content/v1/570c1514c2ea513f7d99d605/1501176181517-QDJMWR0I5H0PR2WQ7B2R/image-asset.jpeg?format=1000w" alt="">
+        <p>L'esercito inglese, impegnato a liberare le truppe alleate dalla Francia occupata dai nazisti, 
+            resta intrappolato sulla spiaggia con le spalle al mare e devone affrontare una situazione impossibile mentre il nemico avanza.</p>
+    </div>
+    
+    <div class="column">
+    <h2>Inizia a guardare: 'Batman Begins'</h2>
+        <img src="https://cdn.gelestatic.it/kataweb/tvzap/2018/04/Batman-Begins_1000.jpg" alt="">
+        <p >Traumatizzato dalla morte dei genitori, il giovane Bruce Wayne intraprende un 
+            addestramento fisico e psicologico con il suo mentore per prepararsi alla lotta contro il male nella città di Gotham City.</p>
+    </div>
+    </div>
     
     <div class="buttonspan">
-        <img src="https://upload.wikimedia.org/wikipedia/it/thumb/4/4a/Oppenheimer_%28film%29.jpg/1200px-Oppenheimer_%28film%29.jpg" alt="">
+        
         <p><input class="button1" type="submit" value="Stream now"></p>
         <p><input class="button2" type="submit" value="All episodes"></p>
     </div>
-    
+   
     <h1>I FILM PIU' POPOLARI DI QUESTA SETTIMANA</h1>
 
         <div class="gallery">
@@ -46,7 +70,21 @@
 
   
     
-</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 </body>
 </html>
