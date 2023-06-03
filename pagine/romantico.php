1a5ec7206ce6a4d6367008816938c77495a0f1b1
Cate <caterina.ferrera@liceobanfi.eu>
2023-06-03 08:51:52 +0200

    cambiamenti 1

--- pagine/romantico.php
+++ pagine/romantico.php
@@ -4,13 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylegeneri.css">
+    <link rel="stylesheet" href="../pagine/stylegeneri.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Genere: Romantico</title>
 </head>
 
 <body>
-    
+<div class="container-pagina">
+            <header>
+                <div class="logo">
+                    <img src="../immagini/logo.jpg" alt="" class="logo">
+                </div>
+                <nav>
+                    <ul>
+                    <li><a href="../pagine/index.php">HOME</a></li> 
+                    <li><a href="#">GENERI</a>
+                    <ul>
+                    <li><a href="../pagine/romantico.php">ROMANTICO</a></li>
+                    <li><a href="../pagine/avventura.php">AVVENTURA</a></li>
+                    <li><a href="../pagine/commedia.php">COMMEDIA</a></li>
+                    <li><a href="../pagine/fantascienza.php">FANTASCENZA</a></li>
+                    <li><a href="../pagine/horror.php">HORROR</a></li>
+                    </ul>
+                    </li>     
+                </nav>
+                <button><a href="../pagine/registrazione.php">ACCEDI</a></button>
+            </header>
+        </div>
     <h1>Genere: Romantico</h1>
 
     <section class="cards">
@@ -85,27 +105,6 @@
             </ul>
             <p>Per maggiori informazioni contattateci al numero: <b> +39 345 023 39999 </b>e scriveteci alle mail: <U>@yahoo.it - GICAandCO@gmail.com</U></p>
         </div>
-        <section class="generi">
-            <div class="homepage">
-                <a href="../index.html">HOME PAGE</a>
-            </div>
-            <div class="chi_siamo">
-                <a href="../pagine/chi_siamo.html">CHI SIAMO?</a>
-            </div>
-            <div class="commedia">
-                <a href="../pagine/commedia.html">COMMEDIA</a>
-            </div>
-            <div class="avventura">
-                <a href="../pagine/avventura.html">AVVENTURA</a>
-            </div>
-           
-            <div class="fantascienza">
-                <a href="../pagine/fantascienza.html">FANTASCENZA</a>
-            </div>
-            <div class="horror">
-                <a href="../pagine/horror.html">HORROR</a>
-            </div>
-        </section>
         <div class="footer__bootom">
             <p>copyright &copy; 2012 GICAandCO. designed by <span>nethunt</span></p>
         </div>
